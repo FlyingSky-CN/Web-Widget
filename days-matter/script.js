051d91127b66bf811f7ee31df6cf6a92f4a77d2b
FlyingSky-CN <me@fsky7.com>
2022-03-04 19:24:45 +0800

    New Widget: Days Matter

---
+++ days-matter/script.js
@@ -0,0 +1,29 @@
+/**
+ * Apply Query
+ */
+
+const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+window.onload = () => {
+
+    /** Card Round-radius */
+    if (getQueryStringByName('round') == 'true')
+        document.getElementById('card').classList.add('round');
+
+    /** Days Matter */
+    let day = getQueryStringByName('day') == '' ?
+        (new Date().getFullYear() + 1) + '/01/01' :
+        getQueryStringByName('day');
+    let name = getQueryStringByName('name') == '' ?
+        (new Date().getFullYear() + 1) + ' 年元旦' :
+        getQueryStringByName('name');
+    let now = new Date().getTime(),
+        target = new Date(day).getTime(),
+        title = target > now ? `${name}还有` : `${name}已经`;
+    let days = Math.ceil(Math.abs(target - now) / 1000 / 60 / 60 / 24),
+        date = day.split('/').join('-'),
+        text = target > now ? '目标日' : '起始日';
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('title').innerHTML = title;
+    document.getElementById('date').innerHTML = `${text}：${date}`;
+}
