a7ba6b36ba7c2dca206d726d64d4e37a430b67a7
FlyingSky-CN <me@fsky7.com>
2022-03-04 19:24:52 +0800

    Update Assets

---
+++ assets/global.js
@@ -0,0 +1,18 @@
+/**
+ * Web Widget | JavaScript
+ */
+
+/**
+ * Get Query String by Name
+ * 
+ * @param {string} name 
+ * @param {string} defaultValue 
+ * @returns 
+ */
+function getQueryStringByName(name, defaultValue = '') {
+    let regExp = new RegExp("[\?\&]" + name + "=([^\&]+)", "i");
+    let regMatch = location.search.match(regExp);
+    if (regMatch == null || regMatch.length < 1)
+        return defaultValue;
+    return regMatch[1];
+}
